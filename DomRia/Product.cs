665a2d08b9461b1da38924afcb2b4532aae5b042
Fili_vw23@student.itstep.org <Fili_vw23@student.itstep.org>
2022-04-21 19:43:17 +0300

    -add manager 50/50

--- DomRia/Product.cs
+++ DomRia/Product.cs
@@ -27,5 +27,6 @@ namespace DomRia
             Size = size;
         }
 
+
     }
 }
---
+++ Manager/System.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    class System
+    {
+        public void addProduct()
+        {
+            if (!Directory.Exists("Products"))
+            {
+                Directory.CreateDirectory("Products");
+            }
+
+            Console.Write("Введите название продукта");
+            string title = Console.ReadLine();
+
+            Console.Write("Введите описание продукта");
+            string description = Console.ReadLine();
+
+            Console.Write("Введите сколько м³: ");
+            uint size = uint.Parse(Console.ReadLine());
+
+            Console.Write("Введите цену продукта: ");
+            ulong uah = ulong.Parse(Console.ReadLine());
+
+            Console.Write("Введите расположение продукта: ");
+            string adres = Console.ReadLine();
+
+            Console.Write("Введите название риелтора");
+            string titleRieltor = Console.ReadLine();
+
+            Console.Write("Введите описание риелтора");
+            string descriptionRieltor = Console.ReadLine();
+
+            Console.Write("Введите рейтинг риелтора");
+            uint rate = uint.Parse(Console.ReadLine());
+
+            Console.Write("Введите номер риелтора");
+            string phoneNumber = Console.ReadLine();
+
+            File.AppendAllText("Products/!DataBase",String.Format("{0}|{1}|{2}|{3}|{4}|{5}|{6}|{7}|{8}",title,description,size,uah,adres,titleRieltor,descriptionRieltor,rate,phoneNumber));
+        }
+
+    }
+}
